2a772e8a8f824438cc3f1e0488ff3a4f1d7c843b
Stasiek <42705773+xxxstasiek@users.noreply.github.com>
2020-10-12 02:35:01 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'bodacious'
+
+game 'gta5'
+
+client_scripts{
+    'config.lua',
+    'client.lua',
+}
+
+server_scripts{
+    'config.lua',
+    'server.lua',
+}
---
+++ server.lua
@@ -0,0 +1,62 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterCommand('dealer', function(source, args, rawcommand)
+    xPlayer = ESX.GetPlayerFromId(source)
+    drugToSell = {
+        type = '',
+        label = '',
+        count = 0,
+        i = 0,
+        price = 0,
+    }
+    for k, v in pairs(Config.drugs) do
+        item = xPlayer.getInventoryItem(k)
+        count = item.count
+        drugToSell.i = drugToSell.i + 1
+        drugToSell.type = k
+        drugToSell.label = item.label
+        
+        if count >= 5 then
+            drugToSell.count = math.random(1, 5)
+        elseif count > 0 then
+            drugToSell.count = math.random(1, count)
+        end
+
+        if drugToSell.count ~= 0 then
+            drugToSell.price = drugToSell.count * v + math.random(1, 300)
+            TriggerClientEvent('stasiek_selldrugsv2:findClient', source, drugToSell)
+            break
+        end
+        
+        if ESX.Table.SizeOf(Config.drugs) == drugToSell.i and drugToSell.count == 0 then
+            xPlayer.showNotification('Nie posiadasz żadnych dragów', 6)
+        end
+    end
+end, false)
+
+RegisterServerEvent('stasiek_selldrugsv2:pay')
+AddEventHandler('stasiek_selldrugsv2:pay', function(drugToSell)
+    xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.removeInventoryItem(drugToSell.type, drugToSell.count)
+    xPlayer.addAccountMoney('black_money', drugToSell.price)
+end)
+
+RegisterServerEvent('stasiek_selldrugsv2:notifycops')
+AddEventHandler('stasiek_selldrugsv2:notifycops', function(drugToSell)
+    TriggerClientEvent('stasiek_selldrugsv2:notifyPolice', -1, drugToSell.coords)
+    TriggerClientEvent("esx_outlawalert:outlawNotify", -1, "^*^110-72 ^r^7Sprzedaż narkotyków przez na " .. drugToSell.zone)
+end)
+
+ESX.RegisterServerCallback('stasiek_selldrugsv2:getPoliceCount', function(source, cb)
+    count = 0
+
+    for k, v in pairs(ESX.GetPlayers()) do
+        xPlayer = ESX.GetPlayerFromId(v.source)
+        if xPlayer.job.name == 'police' then
+            count = count + 1
+        end
+    end
+
+    cb(count)
+end)
